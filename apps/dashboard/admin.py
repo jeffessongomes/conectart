f00358f8f47c20c3682c05f2573df96f8108329c
Lucaslllll <lucascosta12367@gmail.com>
2020-10-20 02:20:35 -0200

    ue

--- apps/dashboard/admin.py
+++ apps/dashboard/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Events, Subscribe_User, Admin, TecDashImages, Our, Client, Comments
+from .models import Events, Subscribe_User, Admin, TecDashImages, Our, Client, Comments, Photos
 
 admin.site.register(Comments)
 admin.site.register(Client)
@@ -8,3 +8,4 @@ admin.site.register(TecDashImages)
 admin.site.register(Events)
 admin.site.register(Subscribe_User)
 admin.site.register(Admin)
+admin.site.register(Photos)
